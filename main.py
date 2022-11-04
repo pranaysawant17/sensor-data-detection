98bdda7b1967ac8edb933bb62fdd48c1001a5c41
Pranay <pranay.sawant@marktine.com>
2022-11-04 14:16:59 +0530

    from avinash github

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from sensor.configuration.mongodb_connection import MongoDBClient
+from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import os,sys
+from sensor.logger import logging
+from sensor.pipeline import training_pipeline
+from sensor.pipeline.training_pipeline import TrainPipeline
 
-if __name__ == "__main__":
-    mongodb_cli=MongoDBClient()
-    print("collection_name:", mongodb_cli.database.list_collection_names())
+if __name__ == '__main__':
+    try:
+        training_pipeline = TrainPipeline()
+        training_pipeline.run_pipeline()
+    except Exception as e:
+        print(e)
+        logging.exception(e)
